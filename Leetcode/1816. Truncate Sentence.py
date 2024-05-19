79cc4fd5c9d6bbfdf08b36a79aba3846e0c2fe4b
Sanket Dave <teknas17@gmail.com>
2024-05-18 20:08:25 -0700

    Create 1816. Truncate Sentence.py

---
+++ Leetcode/1816. Truncate Sentence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        arr = s.split(" ")
+        return " ".join(arr[:k])
