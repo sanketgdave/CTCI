594362e1ab37231857211868182d77b72c701823
Sanket Dave <teknas17@gmail.com>
2024-06-18 18:17:44 -0700

    Create 237. Delete Node in a Linked List.py

---
+++ Leetcode/237. Delete Node in a Linked List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
+        return
+
+# hint
+# How to become another person in the world? Two steps.
+# One, change your appearance to whom you want to be.
+# Two, kill that person.
