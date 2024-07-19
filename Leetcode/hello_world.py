2ce4eb6008d17d3a174373f0b8fe99644d358873
Sanket Dave <teknas17@gmail.com>
2024-07-19 14:40:26 -0700

    Update hello_world.py

--- Leetcode/hello_world.py
+++ Leetcode/hello_world.py
@@ -1,1 +1,14 @@
-print("hello world!")
+sum = 0
+ser_i = 1
+n = int(input("Enter n: "))
+
+for i in range(0,n):
+    ser_i = ser_i + i
+    if ser_i <= n:
+        sum = sum + ser_i**4
+        print("ser_i: ",ser_i)
+        # print("sum: ",sum)
+    else:
+        break
+    
+print("sum: ",sum)
